31f6318a00d90f5b7db6a5a05da805f2c2f6bde2
Joe Currey <rentalgamer91@.com>
2025-07-24 22:11:53 +0100

    add manifest and service worker to install as app

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,20 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('love-notes-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/app.js',
-        '/icon.png',
-        '/manifest.json'
-      ]);
-    })
+const cacheName = "work-tips-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assets))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
